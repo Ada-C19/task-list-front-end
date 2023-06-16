364fde651cb05e4c06150887ace76e984abb6acd
Bella <bellaiam@uw.edu>
2023-06-16 15:03:07 -0700

    state lifting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
+import { useState } from "react";
 
 const TASKS = [
   {
@@ -16,13 +17,38 @@ const TASKS = [
 ];
 
 const App = () => {
+  const [tasks, setTasks] = useState(TASKS);
+  // const newTasks = tasks.map((task) => {
+  //   if (task.id === id) {
+  //     const updateTask = {...task};
+  //   }
+  //   return updateTask
+  // })
+  const handleToggleComplete = (taskId) => {
+    const newTasks = tasks.map((task) => {
+      return (task.id === taskId) ? {...task, isComplete: !task.isComplete} : task;
+    });
+    setTasks(newTasks);
+    console.log(newTasks);
+};
+
+  const handleDeleteTask = (taskId) => {
+    const newTasks = tasks.filter(
+      (task) => task.id !== taskId
+    );
+    setTasks(newTasks);
+  };
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div><TaskList tasks={tasks}
+        handleToggleComplete ={handleToggleComplete}
+        handleDeleteTask={handleDeleteTask}
+        />
+        </div>
       </main>
     </div>
   );
--- src/components/Task.js
+++ src/components/Task.js
@@ -3,27 +3,32 @@ import PropTypes from 'prop-types';
 
 import './Task.css';
 
-const Task = ({ id, title, isComplete }) => {
-  const [complete, setComplete] = useState(isComplete);
-  const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
+const Task = ( props ) => {
+  const { isComplete, title } = props; 
+  // const [complete, setComplete] = useState(props.isComplete);
+  const buttonClass = isComplete ? 'tasks__item__toggle--completed' : '';
 
   return (
     <li className="tasks__item">
       <button
         className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setComplete(!complete)}
+        onClick={() => props.handleToggleComplete(props.id)}
       >
-        {title}
+        {props.title}
       </button>
-      <button className="tasks__item__remove button">x</button>
+      <button className="tasks__item__remove button"
+      onClick={() => props.handleDeleteTask(props.id)}>x</button>
     </li>
   );
 };
 
 Task.propTypes = {
-  id: PropTypes.number.isRequired,
-  title: PropTypes.string.isRequired,
-  isComplete: PropTypes.bool.isRequired,
+  
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    isComplete: PropTypes.bool.isRequired,
+    handleToggleComplete: PropTypes.func.isRequired,
+    handleDeleteTask: PropTypes.func.isRequired
 };
 
 export default Task;
